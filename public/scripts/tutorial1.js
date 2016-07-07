2535c05a4f8553481b39086340579b49f38b2291
Rohan Pande <rohanpande@sanisoft.com>
2016-07-07 10:47:11 +0530

    intial commit

---
+++ public/scripts/tutorial1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var CommentBox = React.createClass({
+  render:function(){
+    return (
+      <div className="commentBox">
+        Hello, World! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
